4c759a1dfd47258c79e37ac3d2f964d08dd3ddc1
Guilherme Scaranse <guiscaranse@gmail.com>
2018-03-15 18:50:18 -0300

    Removido algumas coisas

--- run.py
+++ run.py
@@ -3,9 +3,9 @@ import beaver
 print("Testando primeiro...")
 teste1 = beaver.post.extract("https://g1.globo.com/rj/rio-de-janeiro/noticia/policia-acredita-que-assassinos-da"
                              "-vereadora-marielle-seguiram-o-carro-em-que-ela-estava.ghtml")
-#print(teste1)
+
 print("Testando segundo...")
 teste2 = beaver.post.extract("https://www.sensacionalista.com.br/2018/03/14/temer-diz-que-seu-governo-pode-ser-"
                              "o-melhor-da-historia-do-brasil-e-deixa-humoristas-desempregados/")
-#print(teste2)
-print(beaver.post.search_relatives(teste1['article_title']))
+
+print(beaver.post.search_relatives(teste2['article_title']))
