71d42f4675c3dcd98ed5c2baa03b1312773edc1a
Guilherme Scaranse <guiscaranse@gmail.com>
2018-03-16 15:05:27 -0300

    Separado pesquisas

---
+++ beaver/bing_search.py
@@ -0,0 +1,37 @@
+import pendulum
+from fuzzywuzzy import fuzz
+from py_ms_cognitive import PyMsCognitiveNewsSearch
+
+from beaver.config import settings
+from beaver.exceptions import BeaverError
+from beaver.post import extract
+
+
+def search_relatives(query_str):
+    """
+    Procura no Bing, posts similares com >50 de pontuação no token_sort_ratio limitado a 10 resultados
+    :param query_str: conteúdo a ser buscado (Pode ser um título ou o conteúdo da postagem)
+    :return: dicionário com um dicionário onde 'relatives' é uma array de dicionários dos dados dos posts similares,
+    e 'score' é uma média do token_sort_ratio dos resultados similares.
+    """
+    rel_response = dict(relatives=[])
+    meta_score = 0
+    if "MS_BING_KEY" not in os.environ:
+        raise BeaverError("Chaves da Microsoft devem estar presentes na variável do sistema MS_BING_KEY")
+    try:
+        results = PyMsCognitiveNewsSearch(os.environ.get("MS_BING_KEY"), query_str, custom_params={
+            "mkt": settings['language'], "setLang": settings['language'][:2]}).search(limit=10, format='json')
+    except Exception:
+        raise BeaverError("Não foi possível se comunicar com o Bing, talvez as chaves tenham expirado?")
+    for result in results:
+        if fuzz.token_sort_ratio(query_str, result.name) > 50:
+            meta_score += fuzz.token_sort_ratio(query_str, result.name)
+            dados = extract(result.url)
+            if dados['date'] is None:
+                dados['date'] = pendulum.parse(result.date_published, tz=settings['timezone'])
+            rel_response['relatives'].append(dados)
+    if meta_score > 0:
+        rel_response['score'] = meta_score / len(rel_response['relatives'])
+    else:
+        rel_response['score'] = meta_score
+    return rel_response
--- beaver/gnews_search.py
+++ beaver/gnews_search.py
@@ -1,18 +1,21 @@
+import json
 import urllib.parse
 import urllib.request
-import json
 
+import pendulum
 
 from beaver.config import settings
+from beaver.post import extract
 
 
-def search(string):
+def search_relatives(string):
     """
     Pesquisa as notícias no feed do Google news, o feed passa por uma API que converte RSS para JSON
     Um exemplo de request retornada pode ser visto aqui: https://pastebin.com/Ef1yseg1
     :param string: texto a ser procurado no google news
-    :return: notícias em formato de json (https://pastebin.com/Ef1yseg1)
+    :return: notícias em formato padrão
     """
+    gnews_results = dict(relatives=[])
     feed = "https://news.google.com/news/rss/search/section/q/" + urllib.parse.quote_plus(string) + "/" + \
            urllib.parse.quote_plus(string) + "?hl=" + settings['language'] + "&gl=" + settings['country'] + "&ned=" + \
            settings['language'] + "_" + settings['country'].lower()
@@ -20,4 +23,8 @@ def search(string):
     json_data = "https://api.rss2json.com/v1/api.json?rss_url=" + urllib.parse.quote_plus(feed)
     with urllib.request.urlopen(json_data) as url:
         data = json.loads(url.read().decode())
-        return data['items']
+        for item in data['items']:
+            dados = extract(item['link'])
+            dados['date'] = pendulum.parse(item['pubDate'], tz=settings['timezone'])
+            gnews_results['relatives'].append(dados)
+    return gnews_results
