21b78f507a29747a167e559615e501919d0015e7
Guilherme Scaranse <guiscaranse@gmail.com>
2018-03-16 14:40:37 -0300

    Documentado pesquisa do Google News

--- beaver/gnews_search.py
+++ beaver/gnews_search.py
@@ -7,6 +7,12 @@ from beaver.config import settings
 
 
 def search(string):
+    """
+    Pesquisa as notícias no feed do Google news, o feed passa por uma API que converte RSS para JSON
+    Um exemplo de request retornada pode ser visto aqui: https://pastebin.com/Ef1yseg1
+    :param string: texto a ser procurado no google news
+    :return: notícias em formato de json (https://pastebin.com/Ef1yseg1)
+    """
     feed = "https://news.google.com/news/rss/search/section/q/" + urllib.parse.quote_plus(string) + "/" + \
            urllib.parse.quote_plus(string) + "?hl=" + settings['language'] + "&gl=" + settings['country'] + "&ned=" + \
            settings['language'] + "_" + settings['country'].lower()
