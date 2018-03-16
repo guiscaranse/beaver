69bd531561955eaa9ff6a19102cbac95e509fcdc
Guilherme Scaranse <guiscaranse@gmail.com>
2018-03-16 13:01:18 -0300

    Adicionado média de similaridade entre os posts relativos

--- beaver/post.py
+++ beaver/post.py
@@ -10,29 +10,33 @@ from beaver.exceptions import BeaverError
 settings = dict({'language': "pt-BR", "replacement_charset": "latin1"})
 
 
+def fixcharset(string):
+    text = fix_encoding(string)
+    if "�" in text:
+        text = text.encode(settings['replacement_charset'], "ignore")
+    return text
+
+
 def extract(url):
     g = Goose({'use_meta_language': True, 'target_language': settings['language'].replace("-", "_"),
                'parser_class': 'soup'})
     response = dict()
     artigo = g.extract(url=url)
-    response['article_title'] = fix_encoding(artigo.title)
+    response['article_title'] = fixcharset(artigo.title)
     response['author'] = artigo.authors
     response['domain'] = artigo.domain
     response['date'] = artigo.publish_date
     if len(artigo.cleaned_text) > 0:
-        text = fix_encoding(artigo.cleaned_text)
-        if "�" in text:
-            text = text.encode(settings['replacement_charset'], "ignore")
+        text = fixcharset(artigo.cleaned_text)
     else:
-        text = fix_encoding(artigo.meta_description)
-        if "�" in text:
-            text = text.encode(settings['replacement_charset'], "ignore")
+        text = fixcharset(artigo.meta_description)
     response['text'] = text
     return response
 
 
 def search_relatives(query_str):
     rel_response = dict(relatives=[])
+    meta_score = 0
     if "MS_BING_KEY" not in os.environ:
         raise BeaverError("Chaves da Microsoft devem estar presentes na variável do sistema MS_BING_KEY")
     try:
@@ -42,5 +46,10 @@ def search_relatives(query_str):
         raise BeaverError("Não foi possível se comunicar com o Bing, talvez as chaves tenham expirado?")
     for result in results:
         if fuzz.token_sort_ratio(query_str, result.name) > 50:
+            meta_score += fuzz.token_sort_ratio(query_str, result.name)
             rel_response['relatives'].append(extract(result.url))
+    if meta_score > 0:
+        rel_response['score'] = meta_score / len(rel_response['relatives'])
+    else:
+        rel_response['score'] = meta_score
     return rel_response
