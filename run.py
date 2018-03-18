38e231265f74bd36cb5fcb9819f102002c7a798b
Guilherme Scaranse <guiscaranse@gmail.com>
2018-03-17 23:49:15 -0300

    Atualizado testes

--- run.py
+++ run.py
@@ -1,8 +1,15 @@
 import beaver
 
+# Verdade
 teste1 = beaver.analyse.score("https://g1.globo.com/rj/rio-de-janeiro/noticia/policia-acredita-que-assassinos-da"
                               "-vereadora-marielle-seguiram-o-carro-em-que-ela-estava.ghtml")
 print(teste1)
+
+# Falsa
 teste2 = beaver.analyse.score("https://www.sensacionalista.com.br/2018/03/14/temer-diz-que-seu-governo-pode-ser-o-"
                               "melhor-da-historia-do-brasil-e-deixa-humoristas-desempregados/")
 print(teste2)
+
+# Falsa
+teste3 = beaver.analyse.score("http://www.socialistamorena.com.br/as-129-mulheres-que-morreram/")
+print(teste3)
