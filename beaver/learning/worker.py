885c4e5a2f0fcef9c56364cdd83d3d7e3418328c
Guilherme Scaranse <guiscaranse@gmail.com>
2018-05-17 16:19:54 -0300

    Arrumado pacote de machine learning

--- beaver/learning/worker.py
+++ beaver/learning/worker.py
@@ -1,38 +1,21 @@
 import inspect
 import os
 
-import matplotlib.pyplot as plt
 import pandas
 from sklearn import model_selection
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.ensemble import ExtraTreesClassifier, GradientBoostingClassifier
-from sklearn.linear_model import LogisticRegression
 from sklearn.naive_bayes import GaussianNB
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.svm import SVC
-from sklearn.tree import DecisionTreeClassifier
 
 import beaver
 from beaver.config import headers
 
 model = GaussianNB()
-
-
-def describe():
-    from beaver import learning
-    module_path = os.path.dirname(inspect.getfile(learning))
-    dataset = pandas.read_csv(module_path + "/data/dataset.csv", names=headers)
-    print("Sumary: \n" + str(dataset.shape))
-    print("Head: \n" + str(dataset.head(20)))
-    print("Describe:\n" + str(dataset.describe()))
-    print(dataset.groupby('result').size())
-    pandas.plotting.scatter_matrix(dataset)
-    plt.show()
+module_path = os.path.dirname(inspect.getfile(beaver.learning))
 
 
 def train():
-    from beaver import learning
-    module_path = os.path.dirname(inspect.getfile(learning))
+    """
+    Divide os datasets e insere dentro do modelo definido, treina-o, e disponibiliza para consulta
+    """
     dataset = pandas.read_csv(module_path + "/data/dataset.csv", names=headers)
     # Separar dados de validação, e dados de treino
     array = dataset.values
@@ -40,36 +23,19 @@ def train():
     X = array[:, 0:26]  # Dados
     Y = array[:, 26]  # Resultados
     validation_size = 0.20  # Divide datasets
-    seed = 7  # Semente
     global X_train, X_validation, Y_train, Y_validation
     X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y,
                                                                                     test_size=validation_size,
-                                                                                    random_state=seed)
-
-    scoring = 'accuracy'
-    models = [('LR', LogisticRegression()), ('LDA', LinearDiscriminantAnalysis()), ('KNN', KNeighborsClassifier()),
-              ('CART', DecisionTreeClassifier()), ('NB', GaussianNB()), ('SVM', SVC()),
-              ("ETC", ExtraTreesClassifier()), ("GBC", GradientBoostingClassifier())]
-    # evaluate each model in turn
-    results = []
-    names = []
-    for name, model in models:
-        kfold = model_selection.KFold(n_splits=15, random_state=seed)
-        cv_results = model_selection.cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
-        results.append(cv_results)
-        names.append(name)
-        # msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
-
-
-def predict(data):
+                                                                                    random_state=7)
     model.fit(X_train, Y_train)
-    predictions = model.predict(data)
-    print(predictions)
-    print("% verdadeira: ", round(float(model.predict_proba(data)[:, 1]), 2))
-    print("% falsa: ", round(float(model.predict_proba(data)[:, 0]), 2))
 
 
-def auto_predict(url):
+def predict(url: str) -> list:
+    """
+    Prevê se uma notícia é verdadeira ou falsa (1 para verdadeiro, ou 0 para falsa)
+    :param url: a URL a ser analisada
+    :return: Retorna uma lista de probabilidade (1° coluna: chance de ser verdadeira, 2° coluna: chance de ser falsa)
+    """
     train()
     info = dict()
     for head in headers:
@@ -89,5 +55,4 @@ def auto_predict(url):
     for value in info.values():
         planet.append(value)
     universe.append(planet)
-    print(universe)
-    predict(universe)
+    return model.predict_proba(universe)
